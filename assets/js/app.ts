a9d0f8121091cd251fd7495c325ef3e988607f94
Edison Arango <edisonarab@gmail.com>
2023-04-20 01:31:15 -0500

    Config/Typescript

---
+++ assets/js/app.ts
@@ -0,0 +1,6 @@
+import '../scss/app.scss'
+
+// Typescript syntax is supported!, rename this file to app.js if you don't want to use typescript
+const message: string = 'Typescript and Webpack are working!'
+// Webpack is working!
+console.log(message)
